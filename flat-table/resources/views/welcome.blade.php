9085d95109884f2ad87aec522746b59a08dc7412
fatihohn <fatihohn@gmail.com>
2020-08-31 20:37:34 +0900

    init test

--- flat-table/resources/views/welcome.blade.php
+++ flat-table/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    평상도록
                 </div>
 
                 <div class="links">
