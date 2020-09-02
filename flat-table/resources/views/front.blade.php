d4aaf22dc940e2b8d4836b30aa067e9385758bc4
fatihohn <fatihohn@gmail.com>
2020-09-02 22:00:25 +0900

    layout

--- flat-table/resources/views/front.blade.php
+++ flat-table/resources/views/front.blade.php
@@ -4,14 +4,16 @@
     <div class="intro flex-center position-ref">
         <div class="intro_slide">
             <div class="intro_slide_img">
-            
+                <div class="intro_slide_btn down">
+                    <img src="/img/down_btn.png" alt="down_btn">
+                </div>
             </div>
         </div>
     </div>
     <script>
         document.querySelector(".header").setAttribute("class", "header full-height");
         document.querySelector(".menu").style.visibility = "hidden";
-        // document.querySelector(".title").setAttribute("class", "title header-margin title_super");
+        document.querySelector(".title").setAttribute("class", "title header-margin title_super");
         // document.querySelector(".title").style.fontSize = "3.8rem";
         // document.querySelector(".title").style.position = "relative";
         // document.querySelector(".title").style.top = "30px";
