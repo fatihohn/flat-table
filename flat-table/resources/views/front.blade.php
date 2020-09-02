37e5bd248a2e0e1035afdb0311ccf02339640a5c
fatihohn <fatihohn@gmail.com>
2020-09-02 23:02:36 +0900

    layout

--- flat-table/resources/views/front.blade.php
+++ flat-table/resources/views/front.blade.php
@@ -1,21 +1,21 @@
 @extends ('layout')
 
-@section ('intro')
-<!-- <script>
-    document.querySelector(".header").setAttribute("class", "header full-height");
-    document.querySelector(".menu").style.visibility = "hidden";
-    document.querySelector(".title").setAttribute("class", "title header-margin title_super");
-    // document.querySelector(".title").style.fontSize = "3.8rem";
-    // document.querySelector(".title").style.position = "relative";
-    // document.querySelector(".title").style.top = "30px";
-</script> -->
-@endsection
 
 @section ('content')
 
 <div class="intro">
     <div class="intro_slide">
         <div class="intro_slide_img">
+            <div class="intro_slide_btn prev_btn">
+                <a href="#">
+                    <img src="/img/prev_btn.png" alt="prev_btn">
+                </a>
+            </div>
+            <div class="intro_slide_btn next_btn">
+                <a href="#">
+                    <img src="/img/next_btn.png" alt="next_btn">
+                </a>
+            </div>
             <div class="intro_slide_btn down_btn">
                 <a href="#">
                     <img src="/img/down_btn.png" alt="down_btn">
