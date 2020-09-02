c36c9f94c70cb4115eceb867912cbd06b65ec0b8
fatihohn <fatihohn@gmail.com>
2020-09-02 20:38:32 +0900

    layout

--- flat-table/resources/views/front.blade.php
+++ flat-table/resources/views/front.blade.php
@@ -1,21 +1,27 @@
 @extends ('layout')
 
 @section ('intro')
-    <div>
+    <div class="intro flex-center position-ref">
+        <div class="intro_slide">
 
+        </div>
     </div>
     <script>
         document.querySelector(".header").setAttribute("class", "header full-height");
+        document.querySelector(".menu").style.visibility = "hidden";
+        document.querySelector(".title").style.fontSize = "3.8rem";
+        document.querySelector(".title").style.position = "relative";
+        document.querySelector(".title").style.top = "50px";
     </script>
 @endsection
 
 @section ('content')
 
-    <div class="flex-center position-ref full-height intro">
+    <!-- <div class="flex-center position-ref full-height intro">
         <div>
 
         </div>
-    </div>
+    </div> -->
 
 @endsection
 
