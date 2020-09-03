440f0f47396a932fb378863ab9b1ef36b377038f
fatihohn <fatihohn@gmail.com>
2020-09-03 17:37:24 +0900

    front

--- flat-table/resources/views/front.blade.php
+++ flat-table/resources/views/front.blade.php
@@ -14,12 +14,12 @@
                 <!--대표 평상 이미지 목록 끝-->
             <div class="intro_slide_img"><!--배경 이미지 넣기-->
                 <div class="intro_slide_btn prev_btn">
-                    <a href="#">
+                    <a>
                         <img src="/img/prev_btn.png" alt="prev_btn">
                     </a>
                 </div>
                 <div class="intro_slide_btn next_btn">
-                    <a href="#">
+                    <a>
                         <img src="/img/next_btn.png" alt="next_btn">
                     </a>
                 </div>
