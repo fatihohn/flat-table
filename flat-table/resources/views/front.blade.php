909c684b9c6197a06c56cfee2eb5a116ecea3122
fatihohn <fatihohn@gmail.com>
2020-09-03 12:49:07 +0900

    front

--- flat-table/resources/views/front.blade.php
+++ flat-table/resources/views/front.blade.php
@@ -5,6 +5,11 @@
 
 <div class="intro">
     <div class="intro_slide">
+        @foreach<!--대표 평상 이미지 목록-->
+            <div class="intro_slide_imgs" style="visibility:hidden">
+                <img src="" alt="">
+            </div>
+        @endforeach
         <div class="intro_slide_img"><!--배경 이미지 넣기-->
             <div class="intro_slide_btn prev_btn">
                 <a href="#">
