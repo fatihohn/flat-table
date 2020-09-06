3443edb4589e1e6540a9d7cf7ea4f25173eec0c6
fatihohn <fatihohn@gmail.com>
2020-09-06 15:40:30 +0900

    front

--- flat-table/resources/views/front.blade.php
+++ flat-table/resources/views/front.blade.php
@@ -564,7 +564,7 @@
             setTimeout(() => {
                 showIntroTitle(slideImgSrc[0]);
                 
-            }, 300);
+            }, 1000);
             prevBtn.onclick = function() {
                 showPrevImg(prevBtn.classList.item(2));
             }
@@ -593,7 +593,7 @@
                 }
                 setTimeout(() => {
                     showIntroTitle(nextImg);
-                }, 300);
+                }, 1000);
             }
             function showPrevImg(srcNumber) {
                 let prevImg = document.querySelector(".slide_img_src."+CSS.escape(srcNumber));
@@ -620,7 +620,7 @@
                 }
                 setTimeout(() => {
                     showIntroTitle(prevImg);
-                }, 300);
+                }, 1000);
 
 
             }
