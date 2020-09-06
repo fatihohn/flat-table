6dae3ef9addcb473dbe5c2ff8da8ae77a8500c44
fatihohn <fatihohn@gmail.com>
2020-09-07 00:48:02 +0900

    article

--- flat-table/resources/views/article.blade.php
+++ flat-table/resources/views/article.blade.php
@@ -164,16 +164,19 @@
         // }
         // window.addEventListener("resize", organizePics);
         window.addEventListener("resize", function() {
+            // setTimeout(function() {
+            //     if(window.innerWidth > 720) {
+            //         let picControl = setInterval(organizePics, 200);
+            //         setTimeout(() => {
+            //             clearInterval(picControl);
+            //             organizePics();
+            //         }, 200);
+            //     } else {
+            //         organizePics();
+            //     }
+            // }, 300);
             setTimeout(function() {
-                if(window.innerWidth > 720) {
-                    let picControl = setInterval(organizePics, 200);
-                    setTimeout(() => {
-                        clearInterval(picControl);
-                        organizePics();
-                    }, 200);
-                } else {
-                    organizePics();
-                }
+                organizePics();
             }, 300);
         });
 
