75a08efd559ddc11b233d33b006efd07a191615f
fatihohn <fatihohn@gmail.com>
2020-09-06 14:20:27 +0900

    front

--- flat-table/resources/views/article.blade.php
+++ flat-table/resources/views/article.blade.php
@@ -3,69 +3,12 @@
 
 @section ('content')
 
-    <aside class="intro">
-        <div class="intro_slide">
-            <!--대표 평상 이미지 목록-->
-                <div class="intro_slide_imgs" style="visibility:hidden; height:0;">
-                    <img class="slide_img_src" title="평상 일러스트 넣고 싶다" src="https://www.doongdoong.org/uploads/thumbs/1593343384.jpeg" alt="">
-                    <img class="slide_img_src" title="못골 고개에는 평상이 없다" src="https://www.doongdoong.org/se2/upload/c_modify_20200706093011525244320FUN_4974_001.jpg" alt="">
-                    <img class="slide_img_src" title="우물물은 시원하다" src="https://www.doongdoong.org/se2/upload/c_modify_20200706093043948349794IMG_0913.JPG" alt="">
-                </div>
-                <!--대표 평상 이미지 목록 끝-->
-            <div class="intro_slide_img"><!--배경 이미지 넣기-->
-                <div class="intro_slide_btn prev_btn">
-                    <a>
-                        <img src="/img/prev_btn.png" alt="prev_btn">
-                    </a>
-                </div>
-                <div class="intro_slide_btn next_btn">
-                    <a>
-                        <img src="/img/next_btn.png" alt="next_btn">
-                    </a>
-                </div>
-                <div class="intro_slide_btn down_btn">
-                    <a>
-                        <img src="/img/down_btn.png" alt="down_btn">
-                    </a>
-                </div>
-                <header class="intro_slide_title">
-                    <h1 class="slide_title">
-                        <a href="/article">
-                            못골에는 평상이 없더라
-                        </a>
-                    </h1>
-                    <a href="/article" class="slide_enter button">
-                        <p>
-                            평상 살펴보기
-                        </p>
-                    </a>
-                </header>
-            </div>
-        </div>
-    </aside>
-    <div class="spacer"></div>
-    <aside class="tagline">
-        <div class="container group">
-            <div class="col-1">
-                <h1>
-                    동두천<em>의</em>
-                    평상
-                </h1>
-            </div>
-            <div class="col-2">
-                <h2>변방평상</h2>
-                <p>
-                    평상을 이용하는 사람들의 문화를 관찰합니다.<br>
-                    평상이 품은 역사와 특유의 문화를 배웁니다.
-                </p>
-            </div>
-        </div>
-    </aside>
+    
         <!-- 평상 리스트 -->
     <section class="front_main">
         <div class="main">
             <div class="container group">
-                <header class="container_header">
+                <header class="container_header articles_header">
                     <img src="/img/flat_table_icon.svg" alt="flat_table_icon">
                     <h1>평상들</h1>
                 </header>
@@ -526,108 +469,6 @@
     </section>
 
     <script>
-        function scrollDown() {
-            let downBtn = document.querySelector(".down_btn");
-            let pageHeight = window.innerHeight;
-            downBtn.addEventListener("click", function() {
-                window.scrollBy(0, pageHeight);
-            });
-        }
-        scrollDown();
-
-        function opacityByScroll() {
-            let scrollPosition = window.pageYOffset;
-            let pageHeight = window.innerHeight;
-            let slideImg = document.querySelector(".intro_slide_img");
-            slideImg.style.opacity = 1 - scrollPosition/pageHeight;
-        }
-        window.onscroll = function() {
-            opacityByScroll();
-        };
-
-        function setIntroImg() {
-            let introSlide = document.querySelector(".intro_slide_img");
-            let slideImgSrc = document.querySelectorAll(".slide_img_src");
-            let prevBtn = document.querySelector(".prev_btn");
-            let nextBtn = document.querySelector(".next_btn");
-            // prevBtn.setAttribute("id", "0");
-            // nextBtn.setAttribute("id", "0");
-            prevBtn.classList.add(slideImgSrc.length-1);
-            nextBtn.classList.add("1");
-            for(let i = 0; i < slideImgSrc.length; i++) {
-                slideImgSrc[i].classList.add(i);
-                // if(i>0) {
-                //     prevBtn.classList.add(i-1);
-                // } else {
-                //     prevBtn.classList.add(slideImgSrc.length-1);
-                // }
-                // if(i<slideImgSrc.length) {
-                //     nextBtn.classList.add(i+1);
-                // } else {
-                //     nextBtn.classList.add("0");
-                // }
-            }
-            introSlide.style.backgroundImage = "url('"+slideImgSrc[0].src+"')";
-            // introSlide.style.opacity = "1";
-            prevBtn.onclick = function() {
-                showPrevImg(prevBtn.classList.item(2));
-            }
-            nextBtn.onclick = function() {
-                showNextImg(nextBtn.classList.item(2));
-            }
-
-
-            function showNextImg(srcNumber) {
-                let nextImg = document.querySelector(".slide_img_src."+CSS.escape(srcNumber));
-                introSlide.style.backgroundImage = "url('"+nextImg.src+"')";
-                // introSlide.style.opacity = "1";
-                // prevBtn.classList.remove("'"+srcNumber+"'");
-                // nextBtn.classList.remove("'"+srcNumber+"'");
-                prevBtn.classList.remove(prevBtn.classList.item(2));
-                nextBtn.classList.remove(nextBtn.classList.item(2));
-                if(srcNumber > 0 && srcNumber < slideImgSrc.length -1) {
-                    prevBtn.classList.add(parseInt(srcNumber)-1);
-                    nextBtn.classList.add(parseInt(srcNumber)+1);
-                } else if(srcNumber == "0") {
-                    prevBtn.classList.add(slideImgSrc.length-1);
-                    nextBtn.classList.add(parseInt(srcNumber)+1);
-                } else if(srcNumber == slideImgSrc.length -1) {
-                    prevBtn.classList.add(parseInt(srcNumber)-1);
-                    nextBtn.classList.add("0");
-                }
-            }
-            function showPrevImg(srcNumber) {
-                let prevImg = document.querySelector(".slide_img_src."+CSS.escape(srcNumber));
-                introSlide.style.backgroundImage = "url('"+prevImg.src+"')";
-                // introSlide.style.opacity = "1";
-                // nextBtn.classList.remove("'"+srcNumber+"'");
-                if(srcNumber < slideImgSrc.length-1) {
-                    nextBtn.classList.add(parseInt(srcNumber)+1);
-                } else {
-                    nextBtn.classList.add("0");
-                }
-
-                prevBtn.classList.remove(prevBtn.classList.item(2));
-                nextBtn.classList.remove(nextBtn.classList.item(2));
-                if(srcNumber > 0 && srcNumber < slideImgSrc.length -1) {
-                    prevBtn.classList.add(parseInt(srcNumber)-1);
-                    nextBtn.classList.add(parseInt(srcNumber)+1);
-                } else if(srcNumber == "0") {
-                    prevBtn.classList.add(slideImgSrc.length-1);
-                    nextBtn.classList.add(parseInt(srcNumber)+1);
-                } else if(srcNumber == slideImgSrc.length -1) {
-                    prevBtn.classList.add(parseInt(srcNumber)-1);
-                    nextBtn.classList.add("0");
-                }
-
-
-
-            }
-
-
-        }
-        setIntroImg();
-
         function showReadArticle() {
 
             let articles = document.querySelectorAll(".article");
