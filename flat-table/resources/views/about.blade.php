119205617944634ed92ffde566ec3235d8dbcd8e
fatihohn <fatihohn@gmail.com>
2020-09-07 16:21:06 +0900

    about

--- flat-table/resources/views/about.blade.php
+++ flat-table/resources/views/about.blade.php
@@ -4,12 +4,12 @@
 @section ('content')
 
     <section class="article_main">
+        <header class="container_header articles_header">
+            <img src="/img/flat_table_icon.svg" alt="flat_table_icon">
+            <h1>평상으로부터</h1>
+        </header>
         <div class="main">
             <div class="article_container container">
-                <header class="container_header articles_header">
-                    <img src="/img/flat_table_icon.svg" alt="flat_table_icon">
-                    <h1>평상으로부터</h1>
-                </header>
                 
                 <div class="article_pics about_pics">
                     <figure>
