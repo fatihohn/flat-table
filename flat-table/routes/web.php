7b1cf8efabd7537d01016c676377ee3d329c4aa8
fatihohn <fatihohn@gmail.com>
2020-09-30 19:31:53 +0900

    laravel ui

--- flat-table/routes/web.php
+++ flat-table/routes/web.php
@@ -44,3 +44,7 @@ Route::get('/about', function () {
 Route::get('/test', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
